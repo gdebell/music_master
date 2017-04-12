8a47cefa87ae4fe04060a66ffa9f8be9f41ab526
Gina <gdebell@gmail.com>
2017-04-12 08:01:37 -0600

    Music Master is complete

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        playingUrl: '',
+        audio: null,
+        playing: false
+      }
+  }
+
+  playAudio (previewUrl){
+    let audio = new Audio (previewUrl);
+    if (!this.state.playing) {
+      audio.play();
+      this.setState({
+        playing: true,
+        playingUrl: previewUrl,
+        audio
+      })
+    } else {
+      if (this.state.playingUrl === previewUrl) {
+        this.state.audio.pause();
+        this.setState({
+          playing: false
+        })
+      } else {
+        this.state.audio.pause();
+        audio.play();
+        this.setState({
+          playing: true,
+          playingUrl: previewUrl,
+          audio
+        })
+      }
+    }
+  }
   render () {
     //console.log('gallery props', this.props);
     const tracks = this.props.tracks;
@@ -15,12 +50,20 @@ class Gallery extends Component {
             <div
               key={index}
               className="track"
+              onClick={() => this.playAudio(track.preview_url)}
             >
               <img
                 src={trackImg}
                 className="track-img"
                 alt="track"
               />
+              <div className="track-play">
+                <div className="track-play-inner">
+                  {
+                    this.state.playingUrl === track.preview_url ? <span>| |</span> : <span>&#9654;</span>
+                  }
+                </div>
+              </div>
               <p className="track-text">{track.name}</p>
             </div>
           )
